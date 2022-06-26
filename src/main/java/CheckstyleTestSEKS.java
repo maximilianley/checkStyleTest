2de6b42f83a372d48c621278eef68c5814d1dbf3
max <maximilianthomasley@gmail.com>
2022-06-26 15:21:21 -0300

    Initial commit

---
+++ src/main/java/CheckstyleTestSEKS.java
@@ -0,0 +1,8 @@
+public class CheckstyleTestSEKS {
+
+    public static void main(String[] args) {
+        int arschleckkkkken = 3947397;
+        double kd8989JJJJJJshds = (double) (int) (double) arschleckkkkken;
+    }
+
+}
