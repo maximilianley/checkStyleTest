72ca6f4d215500708ee2362f74646ce0b3017b86
max <maximilianthomasley@gmail.com>
2022-06-26 21:07:16 -0300

    another change

--- src/main/java/CheckstyleTestSEKS.java
+++ src/main/java/CheckstyleTestSEKS.java
@@ -1,8 +1,8 @@
 public class CheckstyleTestSEKS {
 
     public static void main(String[] args) {
-        int arschleckggggkkkken = 3947397;
-        double kd8989JJJJJJshds = (double) (int) (double) arschleckggggkkkken;
+        int ssssliaeufhw3948hwp94ghwq3nruoiehiWWUDHUESHF = 3947397;
+        double kd8989JJJJJJshds = (double) (int) (double) ssssliaeufhw3948hwp94ghwq3nruoiehiWWUDHUESHF;
     }
 
 }
